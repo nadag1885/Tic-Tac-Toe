63a6eb4e41d00bc76fd937d71fa90e4cc3b4c572
nadag1885 <nadag1885@gmail.com>
2024-09-03 23:46:10 +0300

    adding restart btn

--- src/Components/GameOver/GameOver.jsx
+++ src/Components/GameOver/GameOver.jsx
@@ -5,7 +5,7 @@ export default function GameOver({winner , handelRestart}){
     return(
         <div className={style.GameOverScreen}>
             <div  className={style.GameOver}>Game Over!</div>
-            {winner && <div className={style.winner}>{winner} is the Winner</div>} 
+            {winner && <div className={style.winner}>{winner} Won!</div>} 
             <button onClick={handelRestart} className={style.restartBtn}>Restart</button>
 
         </div>
